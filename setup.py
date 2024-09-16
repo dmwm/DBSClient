1e6acbd55c55497cf747a2a0cf4539936138a04a
Valentin Kuznetsov <vkuznet@gmail.com>
2024-09-16 14:20:26 -0400

    New version of the package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup
 
 # version of DBSClient in github
-package_version = "4.0.18"
+package_version = "4.0.19"
 
 # Requirements file for pip dependencies
 requirements = "requirements.txt"
